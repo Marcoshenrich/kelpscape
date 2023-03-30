4e7f9f99a3077beee961f59d1478312984b734a2
Marcos <jmarcoshenrich@gmail.com>
2023-03-30 16:58:04 -0700

    intro dynamic transposition complete

--- src/engine/intro.js
+++ src/engine/intro.js
@@ -8,37 +8,47 @@ export default class Intro {
         this.ctx = this.canvas.getContext('2d')
         this.background = new Image()
         this.background.src = './dist/art/intro.png'
-        this.animate()
         this.bgHeight = 648
         this.bgWidth = 1440
         this.xOffset = 0
         this.yOffset = 0
+        this.animate()
     }
 
     animate() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
-
-        this.positionRecalc()    
-        console.log(this.xOffset)
         // this.ctx.drawImage(this.background, this.backgroundPos[0], this.backgroundPos[1], this.arenaWidth, this.arenaHeight)
-        this.ctx.drawImage(this.background, this.xOffset, this.yOffset, this.bgWidth - this.xOffset, this.bgHeight - this.yOffset, 0, 0, this.canvas.width, this.canvas.height)
+        this.swiffler()
         // this.ctx.drawImage(this.background, 0, 0, this.canvas.width, this.canvas.height)
 
 
         requestAnimationFrame(this.animate.bind(this))
     }
 
-    positionRecalc() {
 
 
-        let screenRatio = this.canvas.height / this.canvas.width
-        //need to make it so the virtual space between the undrawn image width == the image ratio of .45
+    swiffler() {
 
 
+
+            let scaleFactor = Math.max(this.canvas.width / this.bgWidth, this.canvas.height / this.bgHeight);
+
+            let newWidth = this.bgWidth * scaleFactor;
+            let newHeight = this.bgHeight * scaleFactor;
+
+            let x = (this.canvas.width / 2) - (newWidth / 2);
+            let y = (this.canvas.height / 2) - (newHeight / 2);
+
+            // When drawing the image, we have to scale down the image
+            // width and height in order to fit within the canvas
+        console.log(x, y, newWidth, newHeight)
+        this.ctx.drawImage(this.background, x, y, newWidth, newHeight);
         
     }
 
 
 
+
 }
+
