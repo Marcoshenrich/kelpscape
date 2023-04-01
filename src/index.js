ef07fc095a7fdfcd830655d374f05e520ec26952
Marcos <jmarcoshenrich@gmail.com>
2023-03-31 21:06:36 -0700

    rotation logic set

--- src/index.js
+++ src/index.js
@@ -8,35 +8,69 @@ canvas.height = window.innerHeight
 canvas.width = window.innerWidth
 
 
-
-let pilot = new Pilot(canvas)
-
+let ctx = canvas.getContext('2d')
 
 
+let image = new Image()
+image.src = './dist/art/otter/otterDownLeft.png'
 
-canvas.addEventListener("mousemove", (e) => {
-    if (pilot.view.input.mouseIsDownAt) {
-        pilot.view.input.dragScreen([e.x, e.y])
-    }
-})
-
-canvas.addEventListener("mousedown", (e) => {
-    pilot.view.input.mouseIsDownAt = [e.x, e.y]
-})
-
-canvas.addEventListener("click", (e) => {
-    pilot.view.input.mouseIsDownAt = false
-
-    let collisionArr = pilot.view.quadtree.findOverlaps(new Rectangle(e.x - pilot.view.offset[0], e.y - pilot.view.offset[1],1,1),{id:null})
-    // if (pilot.intro) {
-    //     pilot.intro.simTransition = true
-    // } else {
-    // }
-})
+let rotObj = {}
+rotObj["rotation"] = 0
 
 
 
-window.addEventListener("resize", (e)=> {
-    canvas.height = e.currentTarget.innerHeight
-    canvas.width = e.currentTarget.innerWidth
-})
+const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // let rotation = rotObj["rotation"]
+    rotateImage(ctx, image, 100, 100, 100, 200, rotObj["rotation"])
+    rotObj["rotation"] += .1
+    requestAnimationFrame(animate)
+}
+
+function rotateImage(ctx, image, x, y, w, h, degrees) {
+    ctx.save();
+    ctx.translate(x + w / 2, y + h / 2);
+    ctx.rotate(degrees * Math.PI / 180.0);
+    ctx.translate(-x - w / 2, -y - h / 2);
+    ctx.drawImage(image, x, y, w, h);
+    ctx.restore();
+}
+
+animate()
+
+
+
+
+
+
+// let pilot = new Pilot(canvas)
+
+
+
+
+// canvas.addEventListener("mousemove", (e) => {
+//     if (pilot.view.input.mouseIsDownAt) {
+//         pilot.view.input.dragScreen([e.x, e.y])
+//     }
+// })
+
+// canvas.addEventListener("mousedown", (e) => {
+//     pilot.view.input.mouseIsDownAt = [e.x, e.y]
+// })
+
+// canvas.addEventListener("click", (e) => {
+//     pilot.view.input.mouseIsDownAt = false
+
+//     let collisionArr = pilot.view.quadtree.findOverlaps(new Rectangle(e.x - pilot.view.offset[0], e.y - pilot.view.offset[1],1,1),{id:null})
+//     // if (pilot.intro) {
+//     //     pilot.intro.simTransition = true
+//     // } else {
+//     // }
+// })
+
+
+
+// window.addEventListener("resize", (e)=> {
+//     canvas.height = e.currentTarget.innerHeight
+//     canvas.width = e.currentTarget.innerWidth
+// })
